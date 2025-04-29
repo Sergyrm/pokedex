cb4c6a7beedfe89272ead27ce3eaeba24898a165
Sergyrm <sergy.r.m@gmail.com>
2025-04-29 15:35:17 +0200

    Refactored main.go & repl.go

--- main.go
+++ main.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"bufio"
-	"os"
-)
-
 func main() {
-	input := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		input.Scan()
-
-		inputString := input.Text()
-		cleanString := cleanInput(inputString)
-		firstWord := cleanString[0]
-		fmt.Println("Your command was:", firstWord)
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -2,7 +2,29 @@ package main
 
 import (
 	"strings"
+	"fmt"
+	"bufio"
+	"os"
 )
+
+func startRepl() {
+	input := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		input.Scan()
+
+		inputString := input.Text()
+		cleanString := cleanInput(inputString)
+
+		if len(cleanString) == 0 {
+			continue
+		}
+
+		firstWord := cleanString[0]
+		fmt.Println("Your command was:", firstWord)
+	}
+}
 
 func cleanInput(text string) []string {
 	text = strings.ToLower(text)
